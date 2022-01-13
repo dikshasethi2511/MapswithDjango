9bd49e550c63b0e53d51156a4a44e438b4eca901
Diksha Sethi <dikshasethi251@gmail.com>
2022-01-14 03:19:33 +0530

    Added maps and markers using longitude and latitude

--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
-#Takes web request -> returns web response
+# Takes web request -> returns web response
+
+from django.shortcuts import render
+import folium
+import geocoder
+
+def index(request):
+    #Getting the location
+    location = geocoder.osm('UK')
+    longitude = location.lng
+    latitude = location.lat
+    countryName = location.country
+    #Creating a map object
+    mapObject = folium.Map(location=[19,-12], zoom_start=2)
+    folium.Marker([5.594, -0.219], tooltip='More for information!', popup='Ghana').add_to(mapObject)
+    folium.Marker([latitude, longitude], tooltip='More for information!', popup=countryName).add_to(mapObject)
+    #Converting to HTML representation
+    mapObject = mapObject._repr_html_()
+    context = { 'mapObject' : mapObject}
+    return render(request, 'index.html', context)
--- mapproject/mapproject/settings.py
+++ mapproject/mapproject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,10 +26,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
-#Registered my map
+# Registered my map
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'map.apps.MapConfig' ,
+    'map.apps.MapConfig',
 ]
 
-#Created a custom AppConfig class
+# Created a custom AppConfig class
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,11 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mapproject.urls'
 
-#Rendering templates
+# Rendering templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mapproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -116,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
